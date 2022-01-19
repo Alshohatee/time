5b3c1b5b2370c81e0dcec20221e8b4005d390883
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-19 00:44:17 -0500

    to update every second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,19 @@
 import React, { useState } from 'react'
 function App() {
   let now = new Date().toLocaleTimeString();
+
   const [time, setTime] = useState( now )
-  // console.log( time );
+  // to update every second 
+  setInterval( updateTime, 1000 );
+
   function updateTime() {
     let newTime = new Date().toLocaleTimeString();
     setTime( newTime )
-
   }
   return (
     <div className="container">
       <h1>{time}   </h1>
-      <button onClick={updateTime}>Get Time</button>
+      {/* <button onClick={updateTime}>Get Time</button> */}
     </div>
   );
 }
